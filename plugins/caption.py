0b029b895de9b875c254e7baf90e633719a434b9
Soebb <87156166+Soebb@users.noreply.github.com>
2021-10-21 13:08:57 +0330

    Update caption.py

--- plugins/caption.py
+++ plugins/caption.py
@@ -78,11 +78,11 @@ async def editing(bot, message):
             channel = str(message.chat.id).replace('-100', '')
             cap = await get_caption(int(channel))
             if message.audio:
-                caption = cap.caption.replace("{filename}", f"{m.file_name}").replace("{artist}", f"{m.performer}").replace("{title}", f"{m.title}").replace("{ext}", f".{m.file_name.rsplit('.', 1)[1]}")
+                caption = cap.caption.replace("{mime_type}", f"{m.mime_type}").replace("{filename}", f"{m.file_name}").replace("{artist}", f"{m.performer}").replace("{title}", f"{m.title}").replace("{ext}", f".{m.file_name.rsplit('.', 1)[1]}")
             elif message.video:
-                caption = cap.caption.replace("{filename}", f"{m.file_name}").replace("{width}", f"{str(m.width)}").replace("{height}", f"{str(m.height)}").replace("{ext}", f".{m.file_name.rsplit('.', 1)[1]}")
+                caption = cap.caption.replace("{mime_type}", f"{m.mime_type}").replace("{filename}", f"{m.file_name}").replace("{width}", f"{str(m.width)}").replace("{height}", f"{str(m.height)}").replace("{ext}", f".{m.file_name.rsplit('.', 1)[1]}")
             elif message.document:
-                caption = cap.caption.replace("{filename}", f"{m.file_name}").replace("{ext}", f".{m.file_name.rsplit('.', 1)[1]}")
+                caption = cap.caption.replace("{mime_type}", f"{m.mime_type}").replace("{filename}", f"{m.file_name}").replace("{ext}", f".{m.file_name.rsplit('.', 1)[1]}")
         except:
             caption = None
             pass
